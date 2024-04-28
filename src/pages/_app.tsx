7be39c05b6b87ef579f8807319247e6186064f67
minkyung00 <newminkyung@gmail.com>
2024-04-29 02:19:41 +0900

    chore: tanstack query v5 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import type { AppProps } from 'next/app';
 import { domAnimation, LazyMotion } from 'framer-motion';
 
 import '@/styles/globals.css';
+import QueryClientProvider from '@/apis/QueryClientProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <LazyMotion features={domAnimation}>
-      <Component {...pageProps} />
-    </LazyMotion>
+    <QueryClientProvider>
+      <LazyMotion features={domAnimation}>
+        <Component {...pageProps} />
+      </LazyMotion>
+    </QueryClientProvider>
   );
 }
