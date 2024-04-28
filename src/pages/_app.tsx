8172d51003ce7e6e6446d8b80823acf75b29f500
minkyung00 <newminkyung@gmail.com>
2024-04-29 01:57:55 +0900

    chore: framer-motion 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app';
+import { domAnimation, LazyMotion } from 'framer-motion';
 
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LazyMotion features={domAnimation}>
+      <Component {...pageProps} />
+    </LazyMotion>
+  );
 }
