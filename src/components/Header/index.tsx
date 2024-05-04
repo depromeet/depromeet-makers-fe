0839d1c8165224d18ddf40ae824a6344e7c91179
minkyung <newminkyung@gmail.com>
2024-05-04 22:09:13 +0900

    [Design System] Header 컴포넌트 (#16)
    * feat: 헤더 컴포넌트 추가
    
    * feat: 헤더 컴포넌트 스토리 추가
    
    * fix: 잘못 들어간 조건식 삭제
    
    * chore: 중복된 스타일 제거

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+
+import theme from '@/styles/theme';
+
+import Icon from '../Icon';
+
+type HeaderProps = {
+  title: string;
+  canBack?: boolean;
+  canClose?: boolean;
+  onClose?: () => void;
+};
+
+export const Header = ({ title, canBack, canClose, onClose }: HeaderProps) => {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  return (
+    <HeaderStyled>
+      <button type="button" onClick={handleBack}>
+        {canBack && <Icon name="arrow-left" />}
+      </button>
+      <Title>{title}</Title>
+      {canClose && (
+        <button type="button" onClick={onClose}>
+          <Icon name="x-icon" color={theme.color.black} width={24} height={24} />
+        </button>
+      )}
+    </HeaderStyled>
+  );
+};
+
+const HeaderStyled = styled.header<Omit<HeaderProps, 'title'>>`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+
+  ${({ theme }) => theme.typo.title1};
+  color: ${({ theme }) => theme.color.gray_900};
+  line-height: 24px;
+`;
