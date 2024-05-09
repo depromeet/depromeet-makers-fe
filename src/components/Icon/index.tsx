ccf278bd5a0c71a711d52e2e908ed66d15c30f99
sumi-0011 <selina2000@naver.com>
2024-05-09 21:53:21 +0900

    feat: admin 에서 사용하는 아이콘 추가

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -7,8 +7,10 @@ import { CheckIcon } from './CheckIcon';
 import { ClipboardCheckIcon } from './ClipboardCheckIcon';
 import { AbsenceFaceIcon, BaseFaceIcon, LatenessFaceIcon } from './FaceIcon';
 import { HomeIcon } from './HomeIcon';
+import { PaperIcon } from './PaperIcon';
 import { RefreshIcon } from './RefreshIcon';
 import { StateIcon } from './StateIcon';
+import { TeamIcon } from './TeamIcon';
 import { UserIcon } from './UserIcon';
 import { XIcon } from './XIcon';
 
@@ -29,6 +31,8 @@ export const IconComponentMap = {
   check: CheckIcon,
   state: StateIcon,
   refresh: RefreshIcon,
+  team: TeamIcon,
+  paper: PaperIcon,
 } as const;
 
 interface Props extends IconComponentProps {
