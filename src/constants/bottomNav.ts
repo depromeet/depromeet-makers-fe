41cba8c3931304d9173a8ebbe0d205dffa87f7b7
sumi-0011 <selina2000@naver.com>
2024-05-09 21:30:07 +0900

    feat: bottom nav, item props로 받도록 수정

---
+++ src/constants/bottomNav.ts
@@ -0,0 +1,14 @@
+import type { IconComponentMap } from '@/components/Icon';
+
+export type NavItemType = {
+  text: string;
+  icon: keyof typeof IconComponentMap;
+  path: string;
+}[];
+
+export const USER_NAV_ITEMS: NavItemType = [
+  // TODO: path 수정 필요
+  { text: '홈', icon: 'home', path: '/' },
+  { text: '일정', icon: 'calendar', path: '/calendar' },
+  { text: '마이페이지', icon: 'user', path: '/my' },
+] as const;
