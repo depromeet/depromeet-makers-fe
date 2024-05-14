3ec003243d5452ce7ed0e48837a7df4d89dd3422
sumi.byun <selina2000@naver.com>
2024-05-14 17:14:13 +0900

    스크롤 위치에 따라 간소화된 헤더를 보여주도록 (#35)
    * feat: 스크롤 가이드에 따라 간소화된 헤더 노출
    
    * fix: dropdown open position get logic fix

--- src/pages/admin/attendance.tsx
+++ src/pages/admin/attendance.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { m } from 'framer-motion';
 import styled from 'styled-components';
 
 import IconButton from '@/components/Button/IconButton';
@@ -9,20 +10,33 @@ import UserItem from '@/features/admin/attendance/UserItem';
 import WeekSelect from '@/features/admin/attendance/WeekSelect';
 
 function AdminAttendancePage() {
+  const { ref, isViewMiniHeader } = useScrollAction();
+
   const [week, setWeek] = useState(1);
   const [team, setTeam] = useState(1);
 
   return (
     <Layout>
       <Main>
-        <TopSection>
-          <WeekSelect value={week} onChange={(week) => setWeek(week)} />
-          <IconButton iconName="state">전체 출석률</IconButton>
-        </TopSection>
-        <TeamSection>
-          <TeamSelect value={team} onChange={(team) => setTeam(team)} />
-        </TeamSection>
-        <UserSection>
+        {isViewMiniHeader ? (
+          <>
+            <MiniHeaderBlank />
+            <MiniHeader initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+              {week}주차 - {team}팀
+            </MiniHeader>
+          </>
+        ) : (
+          <>
+            <TopSection>
+              <WeekSelect value={week} onChange={(week) => setWeek(week)} />
+              <IconButton iconName="state">전체 출석률</IconButton>
+            </TopSection>
+            <TeamSection>
+              <TeamSelect value={team} onChange={(team) => setTeam(team)} />
+            </TeamSection>
+          </>
+        )}
+        <UserSection ref={ref}>
           {DUMMY_DATA.map((data) => (
             <UserItem key={data.id} {...data} />
           ))}
@@ -32,7 +46,55 @@ function AdminAttendancePage() {
   );
 }
 
+// UserSection 컴포넌트가 일정 높이 이상 스크롤이 되면, 상단에 고정된 간소화된 헤더가 보여지도록 구현
+const MINI_HEADER_TRIGGER = 112;
+
+const useScrollAction = () => {
+  const [isViewMiniHeader, setIsViewMiniHeader] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleScroll = useCallback(() => {
+    if (!ref.current) return;
+
+    const top = ref.current.getBoundingClientRect().top;
+
+    setIsViewMiniHeader(top <= MINI_HEADER_TRIGGER);
+  }, []);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
+  return { ref, isViewMiniHeader };
+};
+
 export default AdminAttendancePage;
+
+const MiniHeader = styled(m.header)`
+  height: 64px;
+  text-align: center;
+  line-height: 64px;
+  background-color: ${({ theme }) => theme.color.gray_100};
+  ${({ theme }) => theme.typo.title1};
+  color: ${({ theme }) => theme.color.gray_900};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  z-index: ${({ theme }) => theme.zIndex.header};
+`;
+
+const MiniHeaderBlank = styled.div`
+  height: 112px;
+  background: transparent;
+`;
 
 const Main = styled.main`
   width: 100%;
@@ -104,6 +166,24 @@ const DUMMY_DATA: {
   },
   {
     id: 7,
+    name: '박지민',
+    position: '디자이너',
+    status: ATTENDANCE_STATUS.출석대기,
+  },
+  {
+    id: 8,
+    name: '박지민',
+    position: '디자이너',
+    status: ATTENDANCE_STATUS.출석대기,
+  },
+  {
+    id: 9,
+    name: '박지민',
+    position: '디자이너',
+    status: ATTENDANCE_STATUS.출석대기,
+  },
+  {
+    id: 10,
     name: '박지민',
     position: '디자이너',
     status: ATTENDANCE_STATUS.출석대기,
