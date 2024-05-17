beeab561e94605b4b7f64a9e056b810047b1b587
minkyung00 <newminkyung@gmail.com>
2024-05-17 22:47:51 +0900

    feat: 세션 출석 상태 타입 추가

--- src/types/attendance.d.ts
+++ src/types/attendance.d.ts
@@ -2,6 +2,8 @@ type AttendanceStatus = 'ATTENDANCE_ON_HOLD' | 'ATTENDANCE' | 'ABSENCE' | 'TARDY
 
 type SessionType = 'ONLINE' | 'OFFLINE';
 
+type SessionAttendanceStatus = 'BEFORE_15MINUTE' | 'ON_TIME' | 'AFTER_15MINUTE';
+
 export interface Attendance {
   attendanceId: string;
   generation: number;
