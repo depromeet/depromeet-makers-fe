04e0df6b69481e0f20bd5ebedf16b978127fdaec
minkyung00 <newminkyung@gmail.com>
2024-05-17 10:14:59 +0900

    feat: 출석 현황 컴포넌트에서 응답 값을 props로 받도록 수정

--- src/features/home/Attendance/index.tsx
+++ src/features/home/Attendance/index.tsx
@@ -1,29 +1,13 @@
 import styled from 'styled-components';
 
-import type { CheckedProps } from '@/components/AttendanceStatus';
 import { AttendanceStatus, AttendanceStatusChecked } from '@/components/AttendanceStatus';
+import { type Attendance as AttendanceType } from '@/types/attendance';
 
-// TODO: 응답 값으로 수정 필요
-const attendanceResponse = [
-  { id: 1, week: 1, checked: true, status: 'default' },
-  { id: 2, week: 2, checked: true, status: 'lateness' },
-  { id: 3, week: 3, checked: true, status: 'absent' },
-  { id: 4, week: 4, checked: true, status: 'absent-proof' },
-  { id: 5, week: 5, checked: false, isOffline: true },
-  { id: 6, week: 6, checked: false, isOffline: true },
-  { id: 7, week: 7, checked: false, isOffline: false },
-  { id: 8, week: 8, checked: false, isOffline: false },
-  { id: 9, week: 9, checked: false, isOffline: true },
-  { id: 10, week: 10, checked: false, isOffline: true },
-  { id: 11, week: 11, checked: false, isOffline: true },
-  { id: 12, week: 12, checked: false, isOffline: true },
-  { id: 13, week: 13, checked: false, isOffline: true },
-  { id: 14, week: 14, checked: false, isOffline: true },
-  { id: 15, week: 15, checked: false, isOffline: true },
-  { id: 16, week: 16, checked: false, isOffline: true },
-];
+interface AttendanceProps {
+  attendances: AttendanceType[];
+}
 
-export const Attendance = () => {
+export const Attendance = ({ attendances }: AttendanceProps) => {
   // TODO: 응답 값으로 수정 필요
   const isBefore15Min = true;
 
@@ -36,12 +20,11 @@ export const Attendance = () => {
         </Title>
 
         <Grid>
-          {attendanceResponse.map(({ id, week, checked, isOffline = false, status = 'default' }) =>
-            checked ? (
-              // TODO: 응답 값에 맞게 타입 수정
-              <AttendanceStatusChecked key={id} week={week} variant={status as CheckedProps['variant']} />
+          {attendances.map(({ week, sessionType, attendanceStatus }) =>
+            attendanceStatus === 'ATTENDANCE_ON_HOLD' ? (
+              <AttendanceStatus key={week} week={week} isOffline={sessionType === 'OFFLINE'} />
             ) : (
-              <AttendanceStatus key={id} week={week} isOffline={isOffline} />
+              <AttendanceStatusChecked key={week} week={week} variant={attendanceStatus} />
             ),
           )}
         </Grid>
@@ -53,6 +36,7 @@ export const Attendance = () => {
 const Container = styled.div`
   padding: 24px 20px;
   width: 100%;
+  min-height: 392px;
   border-radius: 12px;
   background-color: ${({ theme }) => theme.color.white};
 
@@ -93,4 +77,5 @@ const Grid = styled.div`
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 10px;
   width: fit-content;
+  min-width: 310px;
 `;
