a6539e43471999904e54dec77c1b2d5e140481a7
minkyung00 <newminkyung@gmail.com>
2024-05-17 21:13:40 +0900

    refactor: api 응답 값에서 data를 반환하도록 수정

--- src/hooks/apis/auth/useSetDefaultPassCord.tsx
+++ src/hooks/apis/auth/useSetDefaultPassCord.tsx
@@ -11,7 +11,8 @@ interface SetDefaultPassCordRequest {
 
 interface SetDefaultPassCordResponse extends AxiosResponse {}
 
-const setDefaultPassCord = async (request: SetDefaultPassCordRequest) => api.post('/v1/auth/default-passcord', request);
+const setDefaultPassCord = async (request: SetDefaultPassCordRequest) =>
+  await api.post<SetDefaultPassCordResponse>('/v1/auth/default-passcord', request);
 
 export const useSetDefaultPassCord = (
   options?: UseMutationOptions<SetDefaultPassCordResponse, CustomError, SetDefaultPassCordRequest>,
