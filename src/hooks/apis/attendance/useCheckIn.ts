3c8265a8431d94ca6faae380ae7a94fb8f1d2b44
minkyung00 <newminkyung@gmail.com>
2024-05-18 22:49:53 +0900

    feat: 출석체크 시 보여지는 스낵바 메세지 수정

--- src/hooks/apis/attendance/useCheckIn.ts
+++ src/hooks/apis/attendance/useCheckIn.ts
@@ -12,6 +12,12 @@ interface CheckInResponse {
   data: unknown;
 }
 
+const SNACKBAR_MESSAGE: Record<string, string> = {
+  '200': '✅ 출석이 완료되었습니다.',
+  AT0006: '📍 세션 장소로 진입해야 출석이 가능합니다.',
+  AT0002: '🚨 출석 인증기간이 초과되었습니다. \n출석 증빙은 담당 운영진에게 문의하세요.',
+};
+
 export const useCheckIn = (options?: UseMutationOptions<CheckInResponse, CustomError>) => {
   const location = useGeolocation();
   const { showSnackBar } = useSnackBar();
@@ -20,13 +26,12 @@ export const useCheckIn = (options?: UseMutationOptions<CheckInResponse, CustomE
     mutationFn: () => api.post<CheckInResponse>(`/v1/check-in`, location),
     ...options,
     onSuccess: (data, ...rest) => {
-      showSnackBar({ message: data.message });
+      showSnackBar({ message: SNACKBAR_MESSAGE[data.code] ?? data.message });
 
       options?.onSuccess?.(data, ...rest);
     },
     onError: (data, ...rest) => {
-      console.log(data);
-      showSnackBar({ message: data.message });
+      showSnackBar({ message: SNACKBAR_MESSAGE[data.code] ?? data.message });
 
       options?.onError?.(data, ...rest);
     },
