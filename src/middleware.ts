d44cb7d2ba6e7c861883951c6d3069a23bfce256
sumi-0011 <selina2000@naver.com>
2024-05-23 15:56:15 +0900

    feat: middleware에서 refresh token을 이용해 access token 재발급 로직 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,32 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 import { COOKIE_KEY } from './constants/cookie';
+import { postAuthRefresh } from './hooks/apis/auth/useAuthRefresh';
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get(COOKIE_KEY.ACCESS_TOKEN);
+  const response = NextResponse.next();
+  const redirect = NextResponse.redirect(new URL('/login', request.url));
 
-  return token ? NextResponse.next() : NextResponse.redirect(new URL('/login', request.url));
+  const accessToken = request.cookies.get(COOKIE_KEY.ACCESS_TOKEN);
+  const refreshToken = request.cookies.get(COOKIE_KEY.REFRESH_TOKEN);
+
+  if (!accessToken && !refreshToken) return redirect;
+  if (!accessToken && refreshToken) {
+    try {
+      const responseToken = await postAuthRefresh({ refreshToken: refreshToken.value });
+
+      response.cookies.set(COOKIE_KEY.ACCESS_TOKEN, responseToken.accessToken);
+      response.cookies.set(COOKIE_KEY.REFRESH_TOKEN, responseToken.refreshToken);
+
+      return response;
+    } catch (error) {
+      return redirect;
+    }
+  }
+
+  return NextResponse.redirect(new URL('/login', request.url));
 }
 
 export const config = {
-  matcher: ['/((?!login).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)'],
 };
