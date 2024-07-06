d88949cc71f0eb88618ac35b1be6fbb368a202e4
김윤호 yunho <kimyouknow9@gmail.com>
2024-07-07 00:28:03 +0900

    feat: auth 패키지 스캐폴딩 (#4)

---
+++ apps/attendance/pages/login.tsx
@@ -0,0 +1,1 @@
+export { LoginPage as default } from '~/pages/login/LoginPage';
---
+++ packages/auth/src/AuthBoundary.tsx
@@ -0,0 +1,4 @@
+export function AuthBoundary() {
+  // TODO: (@kimyouknow) login페이지 없이 인증/인가에러 에러 발생시 fallback 처리
+  return <div>AuthBoundary</div>;
+}
