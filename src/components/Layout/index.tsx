92c98bfade9b29f6a93c940d90385a017f3d4547
minkyung00 <newminkyung@gmail.com>
2025-01-03 17:00:34 +0900

    feat: 라우터 전환 시, 로딩 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,8 +2,23 @@ import type { PropsWithChildren } from 'react';
 import React from 'react';
 import styled from 'styled-components';
 
+import { useRouterLoading } from '@/hooks/useRouterLoading';
+
+import { Spinner } from '../Spinner';
+
 function Layout({ children }: PropsWithChildren) {
-  return <Container>{children}</Container>;
+  const { isLoading } = useRouterLoading();
+
+  return (
+    <Container>
+      {isLoading && (
+        <Overlay>
+          <Spinner />
+        </Overlay>
+      )}
+      {children}
+    </Container>
+  );
 }
 
 export default Layout;
@@ -13,4 +28,16 @@ const Container = styled.div`
   margin: 0 auto;
   width: 100vw;
   max-width: ${({ theme }) => theme.maxWidth};
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
 `;
