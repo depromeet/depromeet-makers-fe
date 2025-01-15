67b9354eff0d864f099238527fb9233a61a11515
minkyung00 <newminkyung@gmail.com>
2025-01-15 11:31:55 +0900

    fix: sentry cli 에러 발생해도 빌드 가능하도록 수정

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,13 @@ const nextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  org: 'depromeet',
+  org: 'sentry',
   project: 'depromeet',
   authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN,
   silent: false,
+  unstable_sentryWebpackPluginOptions: {
+    errorHandler: (err) => {
+      console.warn(`Sentry CLI Plugin Error: ${err.message}`);
+    },
+  },
 });
