6f0c6f3e41b63226fc3c8ffba9f3a8d8961f2e33
sumi-0011 <selina2000@naver.com>
2025-02-03 23:05:54 +0900

    feat: login 페이지 추가

--- apps/admin/src/app/login/page.tsx
+++ apps/admin/src/app/login/page.tsx
@@ -1,3 +1,55 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isButtonDisabled = email === '' || password === '';
+
+  const handleLogin = () => {
+    console.log(email, password);
+  };
+
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <Card className="w-[400px]  rounded-[12px]">
+        <CardHeader>
+          <CardTitle className="text-center text-[20px] text-gray-900 font-[600]">디프만 어드민</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <div className="space-y-2">
+            <Input
+              type="email"
+              placeholder="이메일을 입력해주세요."
+              className="h-11"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="space-y-2">
+            <Input
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+              className="h-11"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Button
+            className="w-full h-[58px] mt-2 font-semibold text-[16px] rounded-[12px]"
+            disabled={isButtonDisabled}
+            onClick={handleLogin}
+          >
+            로그인
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
