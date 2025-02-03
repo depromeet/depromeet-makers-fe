1e75dfba377a201fca42d9f8b05ab28f0a8b0c75
sumi-0011 <selina2000@naver.com>
2025-02-03 22:03:11 +0900

    feat: admin project setting

---
+++ apps/admin/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import '../styles/globals.css';
+
+export const metadata: Metadata = {
+  title: 'Depromeet Makers Admin',
+  description: 'Depromeet Makers Admin',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
