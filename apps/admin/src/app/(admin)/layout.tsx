731e0fd5dd1e73104880745ddd04a0a28127dc3f
newminkyung <newminkyung@gmail.com>
2025-03-19 12:37:18 +0900

    feat: 레이아웃 ui 수정

--- apps/admin/src/app/(admin)/layout.tsx
+++ apps/admin/src/app/(admin)/layout.tsx
@@ -2,17 +2,19 @@ import type { PropsWithChildren } from 'react';
 
 import { AppSidebar } from '@/components/layout/AppSidebar';
 import { Header } from '@/components/layout/Header';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
-const SessionLayout = ({ children }: PropsWithChildren) => {
+const Layout = ({ children }: PropsWithChildren) => {
   return (
     <SidebarProvider>
       <AppSidebar />
 
-      <Header />
-      <div className="pt-[60px] w-full p-4">{children}</div>
+      <SidebarInset>
+        <Header />
+        <div className="w-full p-4">{children}</div>
+      </SidebarInset>
     </SidebarProvider>
   );
 };
 
-export default SessionLayout;
+export default Layout;
