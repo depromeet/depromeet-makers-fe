20f7b9845faa83e3ae29e20481461058d47cacee
newminkyung <newminkyung@gmail.com>
2025-03-20 18:57:03 +0900

    feat: 세션 form 내용을 미리볼 수 있는 preview 컴포넌트

---
+++ apps/admin/src/app/(admin)/session/(component)/session-preview.tsx
@@ -0,0 +1,67 @@
+import { IconLeft } from 'react-day-picker';
+import { useFormContext } from 'react-hook-form';
+import { getDay, getMonth } from 'date-fns';
+
+import { Badge } from '@/components/ui/badge';
+import { H5 } from '@/components/ui/typography';
+
+import type { SessionForm } from '../(data)/session';
+
+export const SessionPreview = () => {
+  const { watch } = useFormContext<SessionForm>();
+
+  const session = watch();
+
+  return (
+    <div className="w-[375px] h-[670px] p-6 mx-auto my-auto bg-white rounded-xl shadow-xl invisible lg:visible">
+      <IconLeft />
+
+      <H5 className="my-[24px]">{`디프만 {00}기 일정`}</H5>
+
+      <SessionItem {...session} />
+    </div>
+  );
+};
+
+// TODO: 추후 공통 ui 패키지로 분리하기
+const SessionItem = ({ week, sessionType, startTime, title, description, place }: SessionForm) => {
+  const isOffline = sessionType === 'OFFLINE';
+  const month = getMonth(startTime);
+  const day = getDay(startTime);
+
+  return (
+    <>
+      <div className="flex flex-col p-[20px] gap-[20px] rounded-xl bg-white border border-gray-200">
+        <div className="flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2">
+            <Badge className="px-2 py-1 rounded-md border-none bg-gray-200 text-gray-500">{week}주차</Badge>
+            <p className="text-[14px] font-semibold">{`${month}월 ${day}일`}</p>
+          </div>
+
+          {isOffline ? (
+            <Badge className="px-2 py-1 rounded-md border-none bg-gray-900 text-white">오프라인</Badge>
+          ) : (
+            <Badge className="px-2 py-1 rounded-md border-none bg-white text-gray-900 border border-gray-900">
+              온라인
+            </Badge>
+          )}
+        </div>
+
+        <div className="flex flex-col">
+          <div className="flex flex-col gap-2">
+            <p className="text-[16px] font-semibold">{title || '세션 이름을 입력해주세요.'}</p>
+            <p className="text-gray-500">{description || '세션 설명을 입력해주세요.'}</p>
+
+            {isOffline && (
+              <button className="flex justify-between items-center py-[12px] my-[8px] rounded-[6px] border border-gray-200 px-4 bg-white text-gray-500">
+                <span className="flex items-center gap-2 text-[12px] text-gray-400">
+                  {place || '장소를 입력해주세요'}
+                </span>
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
