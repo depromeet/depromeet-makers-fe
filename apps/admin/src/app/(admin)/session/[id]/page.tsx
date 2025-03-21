1e72a87802b017d0b461f1e863a0e826a258e69b
newminkyung <newminkyung@gmail.com>
2025-03-21 13:36:41 +0900

    feat: 세션 수정 폼 페이지

--- apps/admin/src/app/(admin)/session/[id]/page.tsx
+++ apps/admin/src/app/(admin)/session/[id]/page.tsx
@@ -1,5 +1,26 @@
+'use client';
+
+import { useMemo } from 'react';
+import { useGetSessionList } from '@depromeet-makers/api';
+
+import { SessionFormSkeleton } from '../(component)/session-form-skeleton';
+
+import { SessionEditForm } from './(component)/session-edit-form';
+
 const SessionDetailPage = ({ params }: { params: { id: string } }) => {
-  return <>{params.id}</>;
+  const sessionId = params.id;
+
+  const { data: sessionList } = useGetSessionList();
+
+  // TODO: 세션 상세 정보 api 개발 후 변경 예정
+  const session = useMemo(() => {
+    return sessionList?.sessions.find((session) => session.sessionId === sessionId) ?? {};
+  }, [sessionList, sessionId]);
+
+  // TODO: suspense 적용
+  if (!session) return <SessionFormSkeleton />;
+
+  return <SessionEditForm session={session} />;
 };
 
 export default SessionDetailPage;
