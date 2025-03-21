2de01a051416a2b54f1a9659f9bc4d236fa8a630
newminkyung <newminkyung@gmail.com>
2025-03-21 16:39:56 +0900

    feat: session 상세 조회 api 적용

--- apps/admin/src/app/(admin)/session/[id]/page.tsx
+++ apps/admin/src/app/(admin)/session/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useMemo } from 'react';
-import { useGetSessionList } from '@depromeet-makers/api';
+import { useGetSessionDetail } from '@depromeet-makers/api';
 
 import { SessionFormSkeleton } from '../(component)/session-form-skeleton';
 
@@ -10,12 +9,7 @@ import { SessionEditForm } from './(component)/session-edit-form';
 const SessionDetailPage = ({ params }: { params: { id: string } }) => {
   const sessionId = params.id;
 
-  const { data: sessionList } = useGetSessionList();
-
-  // TODO: 세션 상세 정보 api 개발 후 변경 예정
-  const session = useMemo(() => {
-    return sessionList?.sessions.find((session) => session.sessionId === sessionId) ?? null;
-  }, [sessionList, sessionId]);
+  const { data: session } = useGetSessionDetail(sessionId);
 
   // TODO: suspense 적용
   if (!session) return <SessionFormSkeleton />;
