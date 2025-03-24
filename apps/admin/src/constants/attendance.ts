7639c363e46c036027542ab54247ef1f74e4f011
newminkyung <newminkyung@gmail.com>
2025-03-24 11:59:16 +0900

    chore: 출석 status 색상 추가

--- apps/admin/src/constants/attendance.ts
+++ apps/admin/src/constants/attendance.ts
@@ -13,6 +13,13 @@ export const ATTENDANCE_STATUS_KR: Record<ATTENDANCE_STATUS, string> = {
   [ATTENDANCE_STATUS.결석]: '결석',
 };
 
+export const ATTENDANCE_STATUS_COLOR: Record<ATTENDANCE_STATUS, string> = {
+  [ATTENDANCE_STATUS.출석대기]: 'bg-gray-100 text-gray-300',
+  [ATTENDANCE_STATUS.출석]: 'bg-green-100 text-green-300',
+  [ATTENDANCE_STATUS.지각]: 'bg-yellow-100 text-yellow-300',
+  [ATTENDANCE_STATUS.결석]: 'bg-red-100 text-red-300',
+};
+
 export const ATTENDANCE_STATUS_LIST = Object.values(ATTENDANCE_STATUS) as ATTENDANCE_STATUS[];
 
 export const CURRENT_GENERATION = 16; // 임시 처리
