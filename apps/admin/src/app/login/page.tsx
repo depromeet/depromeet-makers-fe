dfe1a6cf92300a63207194e431e90dd802f9a8c6
newminkyung <newminkyung@gmail.com>
2025-04-04 17:52:19 +0900

    feat: 로그인 화면 개선

--- apps/admin/src/app/login/page.tsx
+++ apps/admin/src/app/login/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useAuthTest } from '@depromeet-makers/api';
+import { REGEXP_ONLY_DIGITS } from 'input-otp';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp';
+import { H1 } from '@/components/ui/typography';
+
+const LOGIN_CODE_LENGTH = 6;
 
 export default function Login() {
   const router = useRouter();
@@ -20,17 +26,40 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center">
-      <Card className="w-[400px]  rounded-[12px]">
-        <CardHeader>
-          <CardTitle className="text-center text-[20px] text-gray-900 font-[600]">디프만 어드민</CardTitle>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <Button className="w-full h-[58px] mt-2 font-semibold text-[16px] rounded-[12px]" onClick={handleLogin}>
-            로그인
-          </Button>
-        </CardContent>
-      </Card>
+    <div className="flex flex-col min-h-screen bg-gradient-to-br from-[#dff9fb] to-[#ffffff]">
+      <Link href="/" className="sticky top-0 h-16 p-4 font-bold">
+        디프만 메이커스
+      </Link>
+
+      <main className="flex flex-col flex-1 gap-8 items-center justify-center">
+        <H1 className="font-semibold text-3xl lg:text-4xl">디프만 앱으로 로그인</H1>
+
+        <Card className="w-[400px] flex flex-col items-center rounded-2xl p-6">
+          <CardHeader className="text-center p-0">
+            <CardDescription className="text-base text-gray-500">
+              디프만 앱에 표시되어 있는 번호를 입력해주세요
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="space-y-8 w-full">
+            <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS} autoFocus>
+              <InputOTPGroup className="space-x-2">
+                {Array.from({ length: LOGIN_CODE_LENGTH }, (_, index) => (
+                  <InputOTPSlot
+                    key={index}
+                    index={index}
+                    className="w-12 h-12 border-gray-300 rounded-md border-l"
+                    aria-label={`OTP 입력 ${index + 1}`}
+                  />
+                ))}
+              </InputOTPGroup>
+            </InputOTP>
+            <Button onClick={handleLogin} className="w-full">
+              로그인
+            </Button>
+          </CardContent>
+        </Card>
+      </main>
     </div>
   );
 }
